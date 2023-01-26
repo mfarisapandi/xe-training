c4e496ce21ca4827838efb248cc0b53c271a4a5f
farisapandii <mfaris.apandi@gmail.com>
2023-01-26 11:21:02 +0800

    can connect from order to wizard and wand API

--- order/order/src/main/java/com/accenture/order/OrderApplication.java
+++ order/order/src/main/java/com/accenture/order/OrderApplication.java
@@ -2,9 +2,16 @@ package com.accenture.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.*;
+import org.springframework.web.client.*;
 
 @SpringBootApplication
 public class OrderApplication {
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
